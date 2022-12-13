efa9259d199ff7224b517096c936c2ca6380689f
Edwin Alexander Rodriguez <Edwin.Rodriguez@fundacion-jala.org>
2022-12-13 11:51:17 -0500

    funcion duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,41 +1,49 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, ICON, JUMPING, DUCKING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 JUMPING_ACTION = "jumping"
 RUNNING_ACTION = "running"
-DUCKING_ACTION = "duck"
+DUCKING_ACTION = "ducking"
+
 
 class Dinosaur(Sprite):
     Y_POS = 310
     x_POS = 80
     JUMP_VELOCITY = 8.5
-    DUCK_VELOCITY = 2.5
+
     def __init__(self):
         self.image = RUNNING[0]
         self.rect = self.image.get_rect()
         self.rect.x = self.x_POS
         self.rect.y = self.Y_POS
         self.jump_velocity = self.JUMP_VELOCITY
-        self.duck_velocity = self.DUCK_VELOCITY
         self.step = 0
-        
+
         self.action = RUNNING_ACTION
-    
+
     def update(self, user_input):
         if self.action == RUNNING_ACTION:
             self.run()
-        elif self.action == JUMPING_ACTION:  
+        elif self.action == JUMPING_ACTION:
             self.jump()
         elif self.action == DUCKING_ACTION:
             self.duck()
-            
+
         if self.action != JUMPING_ACTION:
-            if user_input[pygame.K_UP] :
+            if user_input[pygame.K_UP]:
                 self.action = JUMPING_ACTION
-            else:  
+            else:
                 self.action = RUNNING_ACTION
+                
+        if self.action != DUCKING_ACTION:
+           if user_input[pygame.K_DOWN]:
+               self.action = DUCKING_ACTION
+           else:  
+               self.action = RUNNING_ACTION
+        
+            
         
         if self.step >=9:
             self.step = 0
@@ -58,7 +66,12 @@ class Dinosaur(Sprite):
        
        
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step <5 else DUCKING[1]
+        self.step +=1
+        self.rect.y = 340
+
+        
+        
         
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
