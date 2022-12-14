c1cabb2e8c2b35b5fa3653b458310f3991004073
Edwin Alexander Rodriguez <Edwin.Rodriguez@fundacion-jala.org>
2022-12-14 11:33:13 -0500

    Cactus Larges

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,16 +1,19 @@
 import pygame
-from dino_runner.components.obstacles.cactus import Cactus
-# from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.cactus import Cactus, CactusLa
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.obstaclesLa = []
         
     def update(self, game):
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(CactusLa(LARGE_CACTUS))
+            
             
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
